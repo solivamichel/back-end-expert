d3de043cf16c434ee0e86a0966d9695da3d9d1f5
solivamichel <michelsolivadev@gmail.com>
2024-12-11 09:13:12 -0300

    feat: add UPDATE USER endpoint

--- user-service-api/src/main/java/br/com/soliva/userserviceapi/controller/impl/UserControllerImpl.java
+++ user-service-api/src/main/java/br/com/soliva/userserviceapi/controller/impl/UserControllerImpl.java
@@ -5,6 +5,7 @@ import br.com.soliva.userserviceapi.entity.User;
 import br.com.soliva.userserviceapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import models.requests.CreateUserRequest;
+import models.requests.UpdateUserRequest;
 import models.responses.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,10 @@ public class UserControllerImpl implements UserController {
     @Override
     public ResponseEntity<List<UserResponse>> findAll() {
         return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @Override
+    public ResponseEntity<UserResponse> update(final String id, final UpdateUserRequest updateUserRequest) {
+        return ResponseEntity.ok().body(userService.update(id, updateUserRequest));
     }
 }
--- user-service-api/src/main/java/br/com/soliva/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/br/com/soliva/userserviceapi/service/UserService.java
@@ -6,6 +6,7 @@ import br.com.soliva.userserviceapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import models.exceptions.ResourceNotFoundException;
 import models.requests.CreateUserRequest;
+import models.requests.UpdateUserRequest;
 import models.responses.UserResponse;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -21,13 +22,23 @@ public class UserService {
     private final UserMapper userMapper;
 
     public UserResponse findById(final String id) {
-        return userMapper.fromEntity(userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Object not found. Id: " + id + ", Type: " + User.class.getSimpleName())));
+        return userMapper.fromEntity(find(id));
     }
 
     public void save(CreateUserRequest createUserRequest) {
         verifyIfEmailAlreadyExists(createUserRequest.email(), null);
         userRepository.save(userMapper.fromRequest(createUserRequest));
+    }
+
+    public UserResponse update(final String id, final UpdateUserRequest updateUserRequest) {
+        User entity = find(id);
+        verifyIfEmailAlreadyExists(updateUserRequest.email(), id);
+        return userMapper.fromEntity(userRepository.save(userMapper.update(updateUserRequest, entity)));
+    }
+
+    private User find(final String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Object not found. Id: " + id + ", Type: " + User.class.getSimpleName()));
     }
 
     private void verifyIfEmailAlreadyExists(final String email, final String id) {
